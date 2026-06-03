637b2ca2bb5aa63f3ede7f5e3160138d7609f4a7
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2026-06-03 23:14:50 +0900

    refactor: payment/queue/reservation Entity를 infrastructure 레이어로 이동

--- src/main/java/kr/hhplus/be/server/queue/infrastructure/entity/QueueTokenEntity.java
+++ src/main/java/kr/hhplus/be/server/queue/infrastructure/entity/QueueTokenEntity.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.queue.domain.entity;
+package kr.hhplus.be.server.queue.infrastructure.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/kr/hhplus/be/server/reservation/infrastructure/entity/ReservationEntity.java
+++ src/main/java/kr/hhplus/be/server/reservation/infrastructure/entity/ReservationEntity.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.reservation.domain.entity;
+package kr.hhplus.be.server.reservation.infrastructure.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
