f5b05ddf4dc71e8cf33849739c6e73c98e5caaee
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2026-06-05 16:03:41 +0900

    test: ConcertSchedule/Seat 서비스 테스트 보완

--- src/test/java/kr/hhplus/be/server/concert/application/ConcertScheduleServiceTest.java
+++ src/test/java/kr/hhplus/be/server/concert/application/ConcertScheduleServiceTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,10 +35,33 @@ public class ConcertScheduleServiceTest {
     @Test
     @DisplayName("성공: 콘서트 예약가능한 날짜 목록 조회")
     void 콘서트_예약_가능_날짜_목록_조회() {
+        // given
+        Long concertId = 1L;
+        
         List<ConcertSchedule> scheduleList = List.of(
-
-
+            ConcertSchedule.create(concertId, LocalDate.now(), LocalTime.now(), 10),
+            ConcertSchedule.create(concertId, LocalDate.of(2026, 06, 07), LocalTime.now(), 15)
         );
+
+        when(concertScheduleRepository.findAvailableDates(concertId)).thenReturn(scheduleList);
+        // when
+        List<ConcertSchedule> result = concertScheduleService.getAvailableDates(concertId);
+    
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result).isEqualTo(scheduleList);
+    }
+
+    @Test
+    @DisplayName("실패: 콘서트 예약가능한 날짜 목록 없음")
+    void 콘서트_예약_가능_날짜_목록_없음() {
+        // given
+        when(concertScheduleRepository.findAvailableDates(1L)).thenReturn(List.of());
+
+        // when&then
+        assertThatThrownBy(() -> concertScheduleService.getAvailableDates(1L))
+        .isInstanceOf(BusinessException.class)
+        .hasFieldOrPropertyWithValue("errorCode", ErrorCode.CONCERT_SCHEDULE_NOT_FOUND);
     }
 
 }
--- src/test/java/kr/hhplus/be/server/seat/application/SeatServiceTest.java
+++ src/test/java/kr/hhplus/be/server/seat/application/SeatServiceTest.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.seat.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -12,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import kr.hhplus.be.server.common.exception.BusinessException;
+import kr.hhplus.be.server.common.exception.ErrorCode;
 import kr.hhplus.be.server.seat.domain.model.Seat;
 import kr.hhplus.be.server.seat.domain.repository.SeatRepository;
 
@@ -29,7 +33,7 @@ public class SeatServiceTest {
     void 예약_가능_좌석_조회() {
         Long scheduleId = 1L;
 
-        List<Seat> seatList = List.of(a
+        List<Seat> seatList = List.of(
             Seat.create(scheduleId, 1, 50000L),
             Seat.create(scheduleId, 2, 50000L)
         );
@@ -40,5 +44,18 @@ public class SeatServiceTest {
 
         assertThat(result).hasSize(2);
         assertThat(result).isEqualTo(seatList);
+    }
+
+    @Test
+    @DisplayName("실패: 콘서트 예약 가능 좌석 없음")
+    void 예약_가능_좌석_없음() {
+        // given
+        Long scheduleId = 1L;
+
+        when(seatRepository.findSeats(scheduleId)).thenReturn(List.of());
+        // when&then
+        assertThatThrownBy(() -> seatService.getSeatList(scheduleId))
+        .isInstanceOf(BusinessException.class)
+        .hasFieldOrPropertyWithValue("errorCode", ErrorCode.SEAT_NOT_FOUND);
     }
 }
